ee034d819d1e09a7a8cf7647629b71ec7bd509d0
zelle <nh.zelletan@gmail.com>
2024-08-23 01:24:02 +0800

    v1.0.0
    added countdown timer

---
+++ src/components/Timer/index.tsx
@@ -0,0 +1,79 @@
+import { useEffect, useRef, useState } from 'react';
+import './index.css';
+
+const Timer = () => {
+    const [timerDays, setTimerDays] = useState('00');
+    const [timerHours, setTimerHours] = useState('00');
+    const [timerMinutes, setTimerMinutes] = useState('00');
+    const [timerSeconds, setTimerSeconds] = useState('00');
+
+    let interval = useRef();
+
+    const stateTimer = () => {
+        const countdownDate = new Date('November 28, 2024 15:30:00').getTime();
+
+        interval = setInterval(() => {
+            const now = new Date().getTime();
+            const distance = countdownDate - now;
+
+            const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+            const hours = Math.floor((distance % (1000 * 60 * 60 * 24) / (1000 * 60 * 60)));
+            const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+            const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+            if (distance < 0){
+                // stop our timer
+                clearInterval(interval.current);
+            } else {
+                // update timer
+                setTimerDays(days);
+                setTimerHours(hours);
+                setTimerMinutes(minutes);
+                setTimerSeconds(seconds);
+            }
+        }, 1000);
+    };
+
+    // componentDidMount
+    useEffect(() => {
+        stateTimer();
+        return() => {
+            clearInterval(interval.current);
+        };
+    });
+
+    return (
+        <section className="bg-[#dad5d1] bg-opacity-50 rounded-lg">
+            <section className="timer">
+                <div>
+                    <span className="mdi mdi-calendar-clock timer-icon"></span>
+                    <h2 className=''>November 28, 2024 Thursday</h2>
+                    <p className=''>Countdown to a really special date. One you could or would never imagine!</p>                    
+                </div>
+                <div className=''>
+                    <section>
+                        <p>{timerDays}</p>
+                        <p><small>Days</small></p>
+                    </section>
+                    <span>:</span>
+                    <section>
+                        <p>{timerHours}</p>
+                        <p><small>Hours</small></p>
+                    </section>
+                    <span>:</span>
+                    <section>
+                        <p>{timerMinutes}</p>
+                        <p><small>Minutes</small></p>
+                    </section>
+                    <span>:</span>
+                    <section>
+                        <p>{timerSeconds}</p>
+                        <p><small>Seconds</small></p>
+                    </section>
+                </div>
+            </section>
+        </section>
+    );
+};
+
+export default Timer;
