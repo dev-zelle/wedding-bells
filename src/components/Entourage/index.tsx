dd2fbf5a923cf850c5616c753d9d673e575b8e2c
Jorge Lim <jorge.lim@hubble.sg>
2024-09-20 22:50:23 +0800

    support swiping for mobile

--- src/components/Entourage/index.tsx
+++ src/components/Entourage/index.tsx
@@ -12,6 +12,9 @@ const Carousel = () => {
 	const [isTransitioning, setIsTransitioning] = useState(false);
 	const [containerWidth, setContainerWidth] = useState(window.innerWidth);
 
+	let touchStartX = 0;
+	let touchEndX = 0;
+
 	// Duplicating the first and last slides to create the loop effect with specified offset
 	const firstThreeSlides = slides.slice(0, offset);
 	const lastThreeSlides = slides.slice(-offset);
@@ -31,6 +34,23 @@ const Carousel = () => {
 		setIsTransitioning(true);
 		setCurrentIndex(prevIndex => prevIndex - 1);
 	}, [isTransitioning]);
+
+	// Handle swipe on mobile
+	const handleTouchStart = useCallback((e: any) => {
+		touchStartX = e.changedTouches[0].screenX;
+	}, []);
+
+	const handleTouchMove = useCallback((e: any) => {
+		touchEndX = e.changedTouches[0].screenX;
+	}, []);
+
+	const handleTouchEnd = useCallback(() => {
+		if (touchStartX - touchEndX > 50) {
+			nextSlide(); // Swiped left, go to next slide
+		} else if (touchEndX - touchStartX > 50) {
+			prevSlide(); // Swiped right, go to previous slide
+		}
+	}, [touchStartX, touchEndX]);
 
 	// Handle direct slide clicks and looping logic
 	const handleClick = (index: number) => {
@@ -91,38 +111,43 @@ const Carousel = () => {
 	}, []);
 
 	return (
-	<div id="entourage" className="w-full py-40 flex flex-col gap-20">
+		<div id="entourage" className="w-full py-40 flex flex-col gap-20">
 		<div className="flex justify-center">
 			<div className="flex flex-col gap-4 px-5">
-				<Titlebar title="Entourage" />
-				<span className="uppercase text-secondary font-oswald font-bold text-[45px]">
-					Meet the Entourage
-				</span>
+			<Titlebar title="Entourage" />
+			<span className="uppercase text-secondary font-oswald font-bold text-[45px]">
+				Meet the Entourage
+			</span>
 			</div>
 		</div>
-		<div className="relative">
+		<div 
+			className="relative"
+			onTouchStart={handleTouchStart}
+			onTouchMove={handleTouchMove}
+			onTouchEnd={handleTouchEnd}
+		>
 			<div
 				className={`flex self-center ${
-				isTransitioning
-					? `transition-transform duration-${transitionDuration}`
-					: "no-transition"
-				}`}
-				style={{
-				transform: `translateX(${
-					centerOffset - currentIndex * slideWidth
-				}px)`,
-				}}
-			>
-				{totalSlides.map((slide, index) => (
-				<div
-					key={index}
-					style={{ minWidth: `${slideWidth}px` }}
-					className={`${
 					isTransitioning
 						? `transition-transform duration-${transitionDuration}`
 						: "no-transition"
-					} flex flex-col items-center hover:opacity-100 ${
-					index === currentIndex
+					}`}
+				style={{
+					transform: `translateX(${
+					centerOffset - currentIndex * slideWidth
+					}px)`,
+				}}
+			>
+			{totalSlides.map((slide, index) => (
+				<div
+					key={index}
+					style={{ minWidth: `${slideWidth}px` }}
+					className={`flex flex-col items-center hover:opacity-100 ${
+						isTransitioning
+							? `transition-transform duration-${transitionDuration}`
+							: "no-transition"
+						} ${
+						index === currentIndex
 						? "scale-110 opacity-100"
 						: "scale-90 opacity-75"
 					}`}
@@ -132,16 +157,16 @@ const Carousel = () => {
 
 					{/* Profile Image (Rounded) */}
 					<img
-					className="w-64 h-64 rounded-full mt-4 border-4 border-white shadow-lg cursor-pointer object-cover"
-					src={slide.image}
-					alt={slide.name}
-					onClick={() => handleClick(index)}
+						className="w-64 h-64 rounded-full mt-4 border-4 border-white shadow-lg cursor-pointer object-cover"
+						src={slide.image}
+						alt={slide.name}
+						onClick={() => handleClick(index)}
 					/>
 
 					{/* Description (Role in the wedding) */}
 					<p className="font-sans text-lg text-teams mt-4">{slide.role}</p>
 				</div>
-				))}
+			))}
 			</div>
 			{/* Navigation Buttons */}
 			<button
@@ -152,8 +177,8 @@ const Carousel = () => {
 				className="absolute top-1/2 right-4 transform -translate-y-1/2 bg-black bg-opacity-50 text-white rounded-full hover:bg-opacity-75 text-2xl p-3"
 				onClick={nextSlide}
 			>▶</button>
+			</div>
 		</div>
-	</div>
 	);
 };
 
